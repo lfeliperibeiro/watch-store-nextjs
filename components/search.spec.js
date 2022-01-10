4f01c57911feb45a308c8ce5d17b2225586ef5c6
Felipe Ribeiro <piporibeiro@gmail.com>
2022-01-10 20:40:37 -0300

    test: call props doSearch when form is submitted

--- components/search.spec.js
+++ components/search.spec.js
@@ -1,10 +1,21 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import Search from './search';
+
+const doSearch = jest.fn();
 
 describe('Search', () => {
   it('should render form', () => {
     render(<Search />);
 
     expect(screen.getByRole('form')).toBeInTheDocument();
+  });
+
+  it('should call props doSearch when form is submitted', async () => {
+    render(<Search doSearch={doSearch} />);
+    const form = screen.getByRole('form');
+
+    fireEvent.submit(form);
+
+    expect(doSearch).toHaveBeenCalledTimes(1);
   });
 });
