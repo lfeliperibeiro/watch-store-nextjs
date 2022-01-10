8fe461f6b28a9e7c8411f40536cfbba3bf5cd30a
Felipe Ribeiro <piporibeiro@gmail.com>
2022-01-10 20:25:32 -0300

    test: render Search component

---
+++ components/serach.spec.js
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react';
+import Search from './search';
+
+describe('Search', () => {
+  it('should render Search component', () => {
+    render(<Search/>)
+
+    expect(screen.getByTestId('search')).toBeInTheDocument()
+  });
+})
