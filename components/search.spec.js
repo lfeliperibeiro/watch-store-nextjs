d9e9d62e6b3946501edefae53653304205dcdaed
Felipe Ribeiro <piporibeiro@gmail.com>
2022-01-11 23:16:30 -0300

    test: call props doSearch with the user input

--- components/search.spec.js
+++ components/search.spec.js
@@ -1,5 +1,6 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 import Search from './search';
+import userEvent from '@testing-library/user-event';
 
 const doSearch = jest.fn();
 
@@ -17,5 +18,18 @@ describe('Search', () => {
     fireEvent.submit(form);
 
     expect(doSearch).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call props doSearch with the user input', async () => {
+    render(<Search doSearch={doSearch} />);
+    const inputText = 'Some text here';
+    const form = screen.getByRole('form');
+    const input = screen.getByRole('textbox');
+
+    await userEvent.type(input, inputText);
+
+    fireEvent.submit(form);
+
+    expect(doSearch).toHaveBeenLastCalledWith(inputText);
   });
 });
