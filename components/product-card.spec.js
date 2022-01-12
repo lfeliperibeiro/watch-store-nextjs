e41982d1a761c0a294672e5376145766fc1aa401
Felipe Ribeiro <piporibeiro@gmail.com>
2022-01-11 23:26:05 -0300

    test: render ProductCard

---
+++ components/product-card.spec.js
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react';
+import ProductCard from './product-card';
+
+describe('ProductCard', () => {
+  it('should render ProductCard', () => {
+    render(<ProductCard />);
+
+    expect(screen.getByTestId('product-card')).toBeInTheDocument();
+  });
+});
