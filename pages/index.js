e43fc301eb8392c953bb89637f857e4446349ff0
Felipe Ribeiro <piporibeiro@gmail.com>
2022-01-18 19:48:47 -0300

    refactor: extract method for the function

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,15 @@ import { useFetchProducts } from '../hooks/use-fetch-product';
 export default function Home() {
   const {products, error} = useFetchProducts()
 
+  function renderProductListOrMessage() {
+    if( products.length === 0 ) {
+      return <h4 data-testid={'no-products'}>No products</h4>
+    }
+    return products.map((product) => (
+      <ProductCard product={product} key={product.id} />
+    ))
+  }
+
   return (
     <main data-testid={'product-list'} className="my-8">
       <Search />
@@ -13,12 +22,7 @@ export default function Home() {
         <h3 className="text-gray-700 text-2xl font-medium">Wrist Watch</h3>
         <span className="mt-3 text-sm text-gray-500">200+ Products</span>
         <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6">
-          {products.length === 0 ? (
-            <h4 data-testid={'no-products'}>No products</h4>
-          ) : null}
-          {products.map((product) => (
-            <ProductCard product={product} key={product.id} />
-          ))}
+          {renderProductListOrMessage()}
         </div>
       </div>
     </main>
