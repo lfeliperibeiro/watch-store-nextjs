de7709225406ecb7ef4a500d2e501a46e0defa41
Felipe Ribeiro <piporibeiro@gmail.com>
2022-01-21 19:27:20 -0300

    feat: display product (singular) when there is only 1 product

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import ProductCard from '../components/product-card';
 import Search from '../components/search';
 import { useFetchProducts } from '../hooks/use-fetch-product';
 import { useEffect, useState } from 'react';
+import Product from '../miragejs/factories/product';
 
 
 export default function Home() {
@@ -38,12 +39,20 @@ export default function Home() {
     return <h4 data-testid="server-error">Server is down</h4>
   }
 
+  function renderProductQuantity() {
+    if(localProducts.length === 1){
+      return   '1 Product'
+    }else {
+      return `${localProducts.length} Products`
+    }
+  }
+
   return (
     <main data-testid={'product-list'} className="my-8">
       <Search doSearch={(term) => setTerm(term)}/>
       <div className="container mx-auto px-6">
         <h3 className="text-gray-700 text-2xl font-medium">Wrist Watch</h3>
-        <span className="mt-3 text-sm text-gray-500">{localProducts.length} Products</span>
+        <span className="mt-3 text-sm text-gray-500">{renderProductQuantity()}</span>
         <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6">
           {renderErrorMessage()}
           {renderProductListOrMessage()}
