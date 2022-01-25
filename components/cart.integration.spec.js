4378c9991341b5004041d8dca2a4f894fe1cab29
Felipe Ribeiro <piporibeiro@gmail.com>
2022-01-25 19:29:58 -0300

    test: add css class "hidden" in  the component

---
+++ components/cart.integration.spec.js
@@ -0,0 +1,33 @@
+import { makeServer } from '../miragejs/server';
+import { renderHook } from '@testing-library/react-hooks';
+import { useCartStore } from '../store/cart';
+import { render, screen } from '@testing-library/react';
+import Cart from './cart';
+
+describe('Cart', () => {
+  let server;
+  let result;
+  let add;
+  let toggle;
+  let reset;
+
+  beforeEach(() => {
+    server = makeServer({environment: 'test'})
+    result = renderHook(() => useCartStore()).result
+    add = result.current.actions.add
+    reset = result.current.actions.reset
+    toggle = result.current.actions.toggle
+    // spy = jest.spyOn(result.current.actions, 'toogle', )
+  })
+
+  afterEach(() => {
+    server.shutdown();
+    jest.clearAllMocks()
+  })
+
+  it('should add css class "hidden" in  the component', () => {
+    render(<Cart/>)
+
+    expect(screen.getByTestId('cart')).toHaveClass('hidden')
+  });
+})
