554083af96a548185714be79c45cf5e479310064
Felipe Ribeiro <piporibeiro@gmail.com>
2022-01-25 20:21:31 -0300

    feat: should call store toggle() twice and display 2 products cards

--- components/cart.integration.spec.js
+++ components/cart.integration.spec.js
@@ -3,10 +3,14 @@ import { renderHook, act } from '@testing-library/react-hooks';
 import { useCartStore } from '../store/cart';
 import { render, screen } from '@testing-library/react';
 import Cart from './cart';
+import userEvent from '@testing-library/user-event';
 
-describe('Cart', () => {
+// todo: verify warning
+
+describe('Cart', (object, method) => {
   let server;
   let result;
+  let spy
   let add;
   let toggle;
   let reset;
@@ -17,7 +21,7 @@ describe('Cart', () => {
     add = result.current.actions.add
     reset = result.current.actions.reset
     toggle = result.current.actions.toggle
-    // spy = jest.spyOn(result.current.actions, 'toogle', )
+    spy = jest.spyOn(result.current.actions, 'toggle')
   })
 
   afterEach(() => {
@@ -26,13 +30,14 @@ describe('Cart', () => {
   })
 
   it('should add css class "hidden" in  the component', () => {
+
     render(<Cart/>)
 
     expect(screen.getByTestId('cart')).toHaveClass('hidden')
   });
 
   it('should not css class "hidden" when using toggle one time', () => {
-    act(() => {
+     act(() => {
       toggle()
     })
 
@@ -40,4 +45,32 @@ describe('Cart', () => {
 
     expect(screen.getByTestId('cart')).not.toHaveClass('hidden')
   });
+
+  it('should call store toggle() twice', () => {
+    render(<Cart/>)
+
+    const button = screen.getByTestId('close-button')
+
+    act(() => {
+      userEvent.click(button)
+      userEvent.click(button)
+    })
+
+
+    expect(spy).toHaveBeenCalledTimes(2)
+  });
+
+  it('should display 2 products cards', () => {
+    const products = server.createList('product', 2)
+
+    act(() => {
+      for(const product of products) {
+        add(product)
+      }
+    })
+
+    render(<Cart/>)
+
+    expect(screen.getAllByTestId('cart-item')).toHaveLength(2)
+  })
 })
