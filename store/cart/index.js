02636a4cefd91f36c81b9feeb90780f266326c41
Felipe Ribeiro <piporibeiro@gmail.com>
2022-01-27 19:06:12 -0300

    feat: create remove and remove all

--- store/cart/index.js
+++ store/cart/index.js
@@ -23,7 +23,8 @@ export const useCartStore = create(set => {
       },
       add(product){
         setState(({state}) => {
-          if(!state.products.includes(product)) {
+          const doesntExists = !state.products.find(({ id }) => id === product.id)
+          if(doesntExists) {
             state.products.push(product)
             state.open = true
           }
